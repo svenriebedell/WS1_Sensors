afaef95f343c0883f2769222066c62b7e95bcf05
Sven Riebe @ Dell <99394991+svenriebedell@users.noreply.github.com>
2022-04-01 13:31:05 +0200

    duplication

--- ws1_dell_secureboot_status_v1_0_1.ps1
+++
@@ -1,7 +0,0 @@
-﻿$secureboot = Switch (Get-CimInstance -Namespace root\dcim\sysman dcim_BIOSEnumeration -Filter "AttributeName='Secure Boot'" | select CurrentValue)
-{ 
-    1 {"Disable"}
-    2 {"Enable"}
-    }
-Write-Output $secureboot
-
