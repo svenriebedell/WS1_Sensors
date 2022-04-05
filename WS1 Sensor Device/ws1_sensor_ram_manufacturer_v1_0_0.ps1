e46b001c402faf5d6b54de9cf6d550a65a0b0458
svenriebedell <sven.riebe@dell.com>
2022-04-05 17:19:03 +0200

    Update

--- WS1 Sensor Device/ws1_sensor_ram_manufacturer_v1_0_0.ps1
+++
@@ -1,2 +0,0 @@
-﻿$ram_manufacturer = Get-CimInstance -Namespace root\dcim\sysman -ClassName DCIM_PhysicalMemory | Select -ExpandProperty Manufacturer
-write-output $ram_manufacturer[0]
--- WS1 Sensor Device/ws1_sensor_ram_speed_v1_0_0.ps1
+++
@@ -1,2 +0,0 @@
-﻿$ram_speed = Get-CimInstance -Namespace root\dcim\sysman -ClassName DCIM_PhysicalMemory | Select -ExpandProperty Speed
-write-output $ram_speed[0]
