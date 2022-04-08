0aecb20648f20b87762c9ef022d48fc4990b1501
svenriebedell <sven.riebe@dell.com>
2022-04-08 11:20:58 +0200

    upload

--- WS1 Multi Value Sensors/WS1_sensor_WMI_BIOS_Security_SystemPW.ps1
+++ WS1 Multi Value Sensors/WS1_sensor_WMI_BIOS_Security_SystemPW.ps1
@@ -1,4 +1,4 @@
-﻿# Returns value for BIOS Security AdminPW, SystemPW, SecureBoot, TPM
+﻿# Returns value for BIOS Security SystemPW
 # Return Type: String
 # Execution Context: System
 # Author: Sven Riebe
@@ -33,12 +33,12 @@ limitations under the License.
 
 <#
 .Synopsis
-   This PowerShell is using WMI Namespace root/DCIM/SYSMAN/wmisecurity -ClassName PasswordObject and -Namespace root/DCIM/SYSMAN/biosattributes -ClassName EnumerationAttribute. Select Value for BIOS Security AdminPW, SystemPW, SecureBoot, TPM.
-   IMPORTANT: This Script support Devices newer than 2018 otherwise BIOS WMI is not supported on Hardware
+   This PowerShell is using WMI WMI Namespace root/DCIM/SYSMAN/wmisecurity -ClassName PasswordObject. Select Value for BIOS Security SystemPW.
+   IMPORTANT: This Script support Devices newer than 2018 otherwise BIOS WMI is not support on Hardware
    IMPORTANT: You need Workspace One UEM and Intelligence to using the full function of this Sensor.
    IMPORTANT: This script does not reboot the system to apply or query system.
 .DESCRIPTION
-   Powershell is using WMI for selcect values of Class WMI Namespace root/DCIM/SYSMAN/wmisecurity -ClassName PasswordObject and -Namespace root/DCIM/SYSMAN/biosattributes -ClassName EnumerationAttribute and handover to Workspace One.You need import this script in the Device / Sensors secetion in Workspace One UEM.
+   Powershell is using WMI for selcect values of Class WMI Namespace root/DCIM/SYSMAN/wmisecurity -ClassName PasswordObject and handover to Workspace One.You need import this script in the Device / Sensors secetion in Workspace One UEM.
    
 #>
 
@@ -63,23 +63,14 @@ function SwitchtoText{
      
 }
 
-#Prepare variables
-$OutputStatement = "Device Details: "
-$Security_AdminPW = "AdminPW is set: "
-$Security_SystemPW = "SystemPW is set: "
-$Security_SecureBoot = "Secureboot is enabled: "
-$Security_TPM = "TPM is enabled: "
-
 
 #select values from WMI Namespace root/DCIM/SYSMAN/wmisecurity -ClassName PasswordObject and -Namespace root/DCIM/SYSMAN/biosattributes -ClassName EnumerationAttribute
 $Security_Password = Get-CimInstance -Namespace root/DCIM/SYSMAN/wmisecurity -ClassName PasswordObject
-$Security_BIOS = Get-CimInstance -Namespace root/DCIM/SYSMAN/biosattributes -ClassName EnumerationAttribute
 
-#select values from $Security_Password and $Security_BIOS
-$Security_AdminPW_Value = $Security_Password | Where-Object {$_.NameID -eq "Admin"} | select -ExpandProperty IsPasswordSet
+
+#select values from $Security_Password
 $Security_SystemPW_Value = $Security_Password | Where-Object {$_.NameID -eq "System"} | select -ExpandProperty IsPasswordSet
-$Security_SecureBoot_Value = $Security_BIOS | Where-Object {$_.AttributeName -eq "SecureBoot"} | select -ExpandProperty CurrentValue
-$Security_TPM_Value = $Security_BIOS | Where-Object {$_.AttributeName -eq "TpmSecurity"} | select -ExpandProperty CurrentValue
+
 
 # change value $Security_AdminPW_Value and $Security_SystemPW_Value from number to text
 $Security_AdminPW_Value = SwitchtoText -Value $Security_AdminPW_Value
@@ -87,6 +78,6 @@ $Security_SystemPW_Value = SwitchtoText -Value $Security_SystemPW_Value
 
 
 #Prepare output string
-$OutputStatement = $OutputStatement+$Security_AdminPW+$Security_AdminPW_Value+" "+$Security_SystemPW+$Security_SystemPW_Value+" "+$Security_SecureBoot+$Security_SecureBoot_Value+" "+$Security_TPM+$Security_TPM_Value
+$OutputStatement = $Security_SystemPW_Value
 
 Write-Output $OutputStatement
