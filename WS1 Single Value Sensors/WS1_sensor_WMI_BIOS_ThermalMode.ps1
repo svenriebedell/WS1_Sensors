5baf7061b66edf38c79088c510e61cd2f48d2b26
Sven Riebe @ Dell <99394991+svenriebedell@users.noreply.github.com>
2022-04-08 16:38:03 +0200

    Update WS1_sensor_WMI_BIOS_ThermalMode.ps1

--- WS1 Single Value Sensors/WS1_sensor_WMI_BIOS_ThermalMode.ps1
+++ WS1 Single Value Sensors/WS1_sensor_WMI_BIOS_ThermalMode.ps1
@@ -33,7 +33,7 @@ limitations under the License.
 
 <#
 .Synopsis
-   This PowerShell is using WMI to collect values from Win32_FAN Class. Select Value for ThermalMode.
+   This PowerShell is using WMI to collect values from Win32_FAN Class. Select Value for FanHealthState, ThermalMode.
    IMPORTANT: You need Workspace One UEM and Intelligence to using the full function of this Sensor.
    IMPORTANT: This script does not reboot the system to apply or query system.
 .DESCRIPTION
@@ -41,9 +41,27 @@ limitations under the License.
    
 #>
 
-#Select value of thermalmode currentvalue
-$BIOS_ThermalManagement_Value = Get-CimInstance -Namespace root/dcim/sysman/biosattributes -ClassName EnumerationAttribute -Filter 'AttributeName="ThermalManagement"' | select -ExpandProperty CurrentValue
+#Check the ChassisType
+$Check_Chassis_Type = Get-CimInstance -ClassName Win32_SystemEnclosure | select -ExpandProperty ChassisTypes
 
-$OutputStatement = $BIOS_ThermalManagement_Value
+if (($Check_Chassis_Type -eq 10) -or ($Check_Chassis_Type -eq 31) -or ($Check_Chassis_Type -eq 32))
+    {
+
+    #Select value of thermalmode currentvalue
+    $BIOS_ThermalManagement_Value = Get-CimInstance -Namespace root/dcim/sysman/biosattributes -ClassName EnumerationAttribute -Filter 'AttributeName="ThermalManagement"' | select -ExpandProperty CurrentValue
+
+    #Prepare Multi string part1
+    $OutputStatement = $BIOS_ThermalManagement_Value+" "
+
+
+    }
+
+Else
+    {
+
+    $OutputStatement = "ThermalMode is not supported on this device"
+
+    }
+
 
 Write-Output $OutputStatement
